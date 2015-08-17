14f9d109ab32b1990d2e6a340f5010d1bfc2cdbb
Igor Belo <igorcoura@gmail.com>
2015-08-16 23:49:14 -0300

    set a language (or not)

--- lib/eagle_search/index.rb
+++ lib/eagle_search/index.rb
@@ -6,12 +6,44 @@ module EagleSearch
     end
 
     def create
-      EagleSearch.client.indices.create index: index_name
+      EagleSearch.client.indices.create index: index_name, body: index_body
+    end
+
+    def delete
+      EagleSearch.client.indices.delete index: index_name
     end
 
     private
     def index_name
-      @settings[:index_name] || @klass.model_name.route_key
+      (@settings[:index_name] || @klass.model_name.route_key).downcase
+    end
+
+    def type_name
+      @klass.model_name.param_key
+    end
+
+    def index_body
+      { mappings: mappings }
+    end
+
+    def mappings
+      base_mappings = {
+        type_name => {
+          properties: {}
+        }
+      }
+
+      columns.select { |column| column.type == :string }.each do |column|
+        base_mappings[type_name][:properties][column.name] = {}
+        base_mappings[type_name][:properties][column.name][:type] = "string"
+        base_mappings[type_name][:properties][column.name][:analyzer] = @settings[:language] if @settings[:language]
+      end
+
+      base_mappings
+    end
+
+    def columns
+      @klass.columns
     end
   end
 end
