d56da4937e73becc98413263c1e5f6af406a4fb9
Igor Belo <igorcoura@gmail.com>
2015-08-21 16:00:00 -0300

    implemented term and terms filters, accepting logical operators between

--- lib/eagle_search/interpreter.rb
+++ lib/eagle_search/interpreter.rb
@@ -6,7 +6,7 @@ module EagleSearch
       query_payload = EagleSearch::Interpreter::Query.new(query).payload
 
       if options[:filters]
-        filter_payload = EagleSearch::Interpreter::Query.new(options[:filters]).payload
+        filter_payload = EagleSearch::Interpreter::Filter.new(options[:filters]).payload
         @payload = { query: { filtered: { query: query_payload, filter: filter_payload } } }
       else
         @payload = { query: query_payload }
