6e6d7b6972af07c1e39e08b2f8e0c61edbc61551
Igor Belo <igorcoura@gmail.com>
2015-09-24 21:51:46 -0300

    added tests for language

---
+++ spec/integration/index_language_spec.rb
@@ -0,0 +1,27 @@
+require "spec_helper"
+
+describe "default settings" do
+  before(:all) do
+    class Product < ActiveRecord::Base
+      include EagleSearch
+      eagle_search language: "portuguese"
+    end
+
+    Product.create_index
+  end
+
+  describe "properties" do
+    let(:properties) do
+      index = Product.eagle_search_index
+      index.mappings[index.type_name][:properties]
+    end
+
+    it "sets portuguese analyzer for name" do
+      expect(properties["name"]).to eq({ index: "analyzed", type: "string", analyzer: "portuguese" })
+    end
+
+    it "sets portuguese analyzer for description" do
+      expect(properties["description"]).to eq({ index: "analyzed", type: "string", analyzer: "portuguese" })
+    end
+  end
+end
