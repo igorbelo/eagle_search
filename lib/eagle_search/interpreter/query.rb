0a5328ab6c1d58131d89a4a50137e7f3c92ad3f9
Igor Belo <igorcoura@gmail.com>
2015-10-22 23:55:02 -0200

    implement an embrionary query interpreter

--- lib/eagle_search/interpreter/query.rb
+++ lib/eagle_search/interpreter/query.rb
@@ -1,8 +1,8 @@
 module EagleSearch
   class Interpreter::Query
 
-    def initialize(klass, query, options)
-      @klass = klass
+    def initialize(index, query, options)
+      @index = index
       @query = query
       @options = options
     end
@@ -13,9 +13,56 @@ module EagleSearch
         if @query == "*"
           { match_all: {} }
         else
-          { multi_match: { query: query, type: "best_fields", fields: ["name"], tie_breaker: 0.3 } }
+          query_payload
         end
       end
+    end
+
+    private
+    def properties
+      @index.mappings[@index.type_name][:properties]
+    end
+
+    def analyzed_properties
+      properties.select { |field_name, field_hash| field_hash[:type] == "string" && field_hash[:index] == "analyzed" }
+    end
+
+    def not_analyzed_properties
+      properties.select { |field_name, field_hash| field_hash[:type] == "string" && field_hash[:index] == "not_analyzed" }
+    end
+
+    def query_payload
+      if analyzed_properties
+        payload = {
+          bool: {
+            should: [
+              {
+                multi_match: {
+                  query: @query,
+                  fields: @options[:fields] || analyzed_properties.keys,
+                  tie_breaker: 0.3
+                }
+              },
+              {
+                bool: {
+                  should: []
+                }
+              }
+            ]
+          }
+        }
+
+        #shingle for analyzed properties
+        analyzed_properties.keys.each do |field_name|
+          payload[:bool][:should][1][:bool][:should] << {
+            match: {
+              "#{ field_name }.shingle" => @query
+            }
+          }
+        end
+      end
+
+      payload
     end
   end
 end
