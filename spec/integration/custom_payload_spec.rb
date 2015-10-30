07646139b3368e449552eea64c3a8e58e134b532
Igor Belo <igorcoura@gmail.com>
2015-10-30 14:40:57 -0200

    added specs for custom payload

---
+++ spec/integration/custom_payload_spec.rb
@@ -0,0 +1,48 @@
+require "spec_helper"
+require "pry"
+
+describe "custom payload" do
+  before(:all) do
+    class Product < ActiveRecord::Base
+      include EagleSearch
+      eagle_search
+
+      def index_data
+        as_json only: [:name, :description, :active, :available_stock]
+      end
+    end
+
+    reindex_products
+  end
+
+  let(:custom_payload) do
+    {
+      query: {
+        filtered: {
+          query: {
+            match_all: {},
+          },
+          filter: {
+            bool: {
+              must_not: [
+                {
+                  term: {
+                    active: false
+                  }
+                }
+              ]
+            }
+          }
+        }
+      }
+    }
+  end
+
+  it "considers the payload over any option" do
+    response = Product.search "something", custom_payload: custom_payload, filters: { available_stock: 300 }
+    expect(response.total_hits).to eq(2)
+    response.hits.each do |hit|
+      expect(hit["_source"]["active"]).to be_truthy
+    end
+  end
+end
