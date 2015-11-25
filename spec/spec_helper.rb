2cd847931dc77b85f0e9e7bace5e1ee9068e09e1
Igor Belo <igorcoura@gmail.com>
2015-11-25 20:31:06 -0200

    added some attributes do aggregate

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,6 +9,7 @@ ActiveRecord::Migration.create_table :products do |t|
   t.string  :name
   t.text    :description
   t.string  :mpn
+  t.string  :category
   t.boolean :active
   t.decimal :list_price
   t.decimal :sale_price
@@ -20,9 +21,9 @@ end
 Product = Class.new(ActiveRecord::Base)
 
 def create_products
-  Product.create(name: "Book: The Good Neighbor", description: "From a phenomenal new voice in suspense fiction comes a book that will forever change the way you look at the people closest to you", active: true, list_price: 14.95, sale_price: 4.99, available_stock: 300, comment: nil)
-  Product.create(name: "Book: The Hidden Child", description: "The brilliant new psychological thriller from worldwide bestseller Camilla Läckberg—the chilling struggle of a young woman facing the darkest chapter of Europe’s past.", active: false, list_price: 8.30, sale_price: 5.20, available_stock: 0)
-  Product.create(name: "Thanth Womens Short Kimono Sleeve Boat Neck Dolman Top", description: "Simply design tunic dress which is basic but stylish.", active: true, list_price: 12.30, sale_price: 9.13, available_stock: 500, comment: "Awesome product")
+  Product.create(name: "Book: The Good Neighbor", category: "Book", description: "From a phenomenal new voice in suspense fiction comes a book that will forever change the way you look at the people closest to you", active: true, list_price: 14.95, sale_price: 4.99, available_stock: 300, comment: nil)
+  Product.create(name: "Book: The Hidden Child", category: "Book", description: "The brilliant new psychological thriller from worldwide bestseller Camilla Läckberg—the chilling struggle of a young woman facing the darkest chapter of Europe’s past.", active: false, list_price: 8.30, sale_price: 5.20, available_stock: 0)
+  Product.create(name: "Thanth Womens Short Kimono Sleeve Boat Neck Dolman Top", category: "Vesture", description: "Simply design tunic dress which is basic but stylish.", active: true, list_price: 12.30, sale_price: 9.13, available_stock: 600, comment: "Awesome product")
 end
 
 def reindex_products(options = { create: true })
