fbf0a8d6194ec04cb1d5d38943a0a4196d7d8453
Igor Belo <igorcoura@gmail.com>
2015-11-25 19:07:24 -0200

    added aggregations support

--- lib/eagle_search/interpreter/aggregation.rb
+++ lib/eagle_search/interpreter/aggregation.rb
@@ -5,25 +5,56 @@ module EagleSearch
     end
 
     def payload
-      payload = {}
-      case @aggregations
-      when Array
-        @aggregations.each do |aggregation|
-          if aggregation.is_a?(String) || aggregation.is_a?(Symbol)
-            payload[aggregation] = build_terms_aggregation(aggregation)
-          end
-        end
-      end
-      payload
+      @payload ||= build_aggregation(@aggregations)
     end
 
     private
+    def build_aggregation(aggregation)
+      payload = {}
+
+      if terms_aggregation?(aggregation)
+        payload[aggregation] = build_terms_aggregation(aggregation)
+      elsif aggregation.is_a?(Array)
+        aggregation.each do |agg|
+          payload.merge!(build_aggregation(agg))
+        end
+      elsif aggregation.is_a?(Hash)
+        field_name, aggregation_body = aggregation.first
+        if stats_aggregation?(aggregation_body)
+          payload = { field_name => build_stats_aggregation(field_name) }
+        else
+          aggregation.each do |key, value|
+            payload.merge!(build_aggregation(key))
+            payload[key].merge!(aggregations: build_aggregation(value))
+          end
+        end
+      end
+
+      payload
+    end
+
     def build_terms_aggregation(field)
       {
         terms: {
           field: field
         }
       }
+    end
+
+    def build_stats_aggregation(field)
+      {
+        stats: {
+          field: field
+        }
+      }
+    end
+
+    def stats_aggregation?(aggregation)
+      aggregation[:type] == "stats"
+    end
+
+    def terms_aggregation?(aggregation)
+      aggregation.is_a?(Symbol) || aggregation.is_a?(String)
     end
   end
 end
